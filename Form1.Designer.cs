0aa11045a460298a2c9ac35df49513b731360aba
RustamZakAs <rostik055@gmail.com>
2018-10-09 23:33:57 +0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace SearchVoenInText
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbText = new System.Windows.Forms.RichTextBox();
+            this.rtbVoen = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDownLen = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownLen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rtbText
+            // 
+            this.rtbText.Location = new System.Drawing.Point(-1, -1);
+            this.rtbText.Name = "rtbText";
+            this.rtbText.Size = new System.Drawing.Size(258, 289);
+            this.rtbText.TabIndex = 0;
+            this.rtbText.Text = "";
+            // 
+            // rtbVoen
+            // 
+            this.rtbVoen.Location = new System.Drawing.Point(309, -2);
+            this.rtbVoen.Name = "rtbVoen";
+            this.rtbVoen.Size = new System.Drawing.Size(256, 289);
+            this.rtbVoen.TabIndex = 1;
+            this.rtbVoen.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(263, 254);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(40, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDownLen
+            // 
+            this.numericUpDownLen.Location = new System.Drawing.Point(263, 12);
+            this.numericUpDownLen.Name = "numericUpDownLen";
+            this.numericUpDownLen.Size = new System.Drawing.Size(40, 20);
+            this.numericUpDownLen.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(564, 289);
+            this.Controls.Add(this.numericUpDownLen);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.rtbVoen);
+            this.Controls.Add(this.rtbText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownLen)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbText;
+        private System.Windows.Forms.RichTextBox rtbVoen;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDownLen;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace SearchVoenInText
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            numericUpDownLen.Value = 10;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int strNumLen = Convert.ToInt32(numericUpDownLen.Value);
+            StringBuilder sb = new StringBuilder(strNumLen);
+            for (int i = 0; i < rtbText.Text.Length; i++)
+            {
+                if (IsNumberContains(rtbText.Text[i].ToString()))
+                {
+                    sb.Append(rtbText.Text[i].ToString());
+                }
+                else if (sb.Length == strNumLen)
+                {
+                    rtbVoen.Text += sb.ToString() + "\n";
+                    sb = new StringBuilder(strNumLen);
+                }
+                else if (sb.Length > 0) sb = new StringBuilder(strNumLen);
+            }
+        }
+
+        static bool IsNumberContains(string input)
+        {
+            foreach (char c in input)
+                if (Char.IsNumber(c))
+                    return true;
+            return false;
+        }
+    }
+}
